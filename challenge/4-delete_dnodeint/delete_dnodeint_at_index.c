21f3f43c9421f6259a6d2a4017fa7a480130ce4a
root <root@DESKTOP-CNB5O32.>
2025-05-13 13:27:26 +0200

    task 4

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -4,50 +4,47 @@
 /**
  * delete_dnodeint_at_index - Delete a node at a specific index from a list
  *
- * @head: A pointer to the first element of a list
+ * @head: A pointer to the pointer to the first element of a list
  * @index: The index of the node to delete
  *
  * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	unsigned int p;
+    dlistint_t *current;
+    unsigned int p;
 
-	if (*head == NULL)
-	{
-		return (-1);
-	}
-	saved_head = *head;
-	p = 0;
-	while (p < index && *head != NULL)
-	{
-		*head = (*head)->next;
-		p++;
-	}
-	if (p != index)
-	{
-		*head = saved_head;
-		return (-1);
-	}
-	if (0 == index)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
-		{
-			tmp->prev = NULL;
-		}
-	}
-	else
-	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
-		*head = saved_head;
-	}
-	return (1);
+    if (!head || !*head)
+        return (-1);
+
+    current = *head;
+    p = 0;
+
+    /* Traverse to the node at index */
+    while (current && p < index)
+    {
+        current = current->next;
+        p++;
+    }
+
+    if (!current)
+        return (-1);
+
+    /* If node to delete is the head */
+    if (current == *head)
+    {
+        *head = current->next;
+        if (*head)
+            (*head)->prev = NULL;
+    }
+    else
+    {
+        if (current->prev)
+            current->prev->next = current->next;
+        if (current->next)
+            current->next->prev = current->prev;
+    }
+
+    free(current);
+    return (1);
 }
